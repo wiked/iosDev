54985cf0bfcf96d923e424b8d802fe5e57c55223
William Krueger <wkrueger@Avectras-Mac-mini.local>
2012-07-04 11:20:27 -0400

    v1.0

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by William Krueger on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
